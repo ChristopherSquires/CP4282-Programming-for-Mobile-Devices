adeb81e361bd5c5b0fc667a744ae1001ee863754
christopher.squires <christopher.squires43@ed.cna.nl.ca>
2025-06-09 11:00:43 -0230

    Assignment 2 start

---
+++ components/Scares.jsx
@@ -0,0 +1,16 @@
+import { Text, View, Image } from "react-native";
+
+export default function Jumpscare( {scareData = {}, image} ) {
+    const { character, game, animatronics = [] } = scareData;
+
+    return (
+        <>
+            <Text>Character: {character}</Text>
+            <Text>Game: {game}</Text>
+            {animatronics.map((animatronic) => (
+                <Text key={animatronic}>{animatronic}</Text>
+            ))}
+            <Image source={image}/>
+        </>
+    );
+}
